d8e33895223da079e6311ca0ed44ba8858823ffb
Dávid Molnár <molnardavid84@gmail.com>
2019-09-24 15:01:26 +0200

    Add demo console xcode app

---
+++ osx sign demo/osx sign demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  osx sign demo
+//
+//  Created by Molnár Dávid on 2019. 09. 24..
+//  Copyright © 2019. zkSNACKs Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
